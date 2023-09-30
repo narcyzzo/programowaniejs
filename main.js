e312e5e76e27be221b340ba33051f20e86ddcffe
narcyzzo <kubavip002@gmail.com>
2023-09-30 14:08:14 +0200

    Done

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-// notatnik z zajęć
-const liczba1 = document.querySelector('#liczba1')
-const btnPrzelicz = document.querySelector('#przelicz')
-const wynikiPojemnik = document.querySelector('#wyniki')
+var input1 = document.getElementById("input1");
+    var input2 = document.getElementById("input2");
+    var input3 = document.getElementById("input3");
+    var input4 = document.getElementById("input4");
 
-btnPrzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML = liczba1.value
-    console.log(liczba1.value)
-})
+    // Pobieramy elementy wynikowe
+    var sumaElement = document.getElementById("suma");
+    var sredniaElement = document.getElementById("srednia");
+    var minimumElement = document.getElementById("minimum");
+    var maksimumElement = document.getElementById("maksimum");
+
+    // Funkcja do obliczania sumy, średniej, minimum i maksimum
+    function oblicz() {
+      var val1 = parseFloat(input1.value) || 0;
+      var val2 = parseFloat(input2.value) || 0;
+      var val3 = parseFloat(input3.value) || 0;
+      var val4 = parseFloat(input4.value) || 0;
+
+      var suma = val1 + val2 + val3 + val4;
+      var srednia = suma / 4;
+      var minimum = Math.min(val1, val2, val3, val4);
+      var maksimum = Math.max(val1, val2, val3, val4);
+
+      sumaElement.textContent = suma;
+      sredniaElement.textContent = srednia;
+      minimumElement.textContent = minimum;
+      maksimumElement.textContent = maksimum;
+    }
+
+    // Dodajemy obsługę zdarzenia input do pól tekstowych
+    input1.addEventListener("input", oblicz);
+    input2.addEventListener("input", oblicz);
+    input3.addEventListener("input", oblicz);
+    input4.addEventListener("input", oblicz);
+    
